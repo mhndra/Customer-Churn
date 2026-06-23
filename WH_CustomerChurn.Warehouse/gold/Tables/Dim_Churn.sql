0b53cef79a53c496875f004c24147cfe1ace4112
Mahendra Nabiha Wisnu <mahendranabiha.2019@student.uny.ac.id>
2026-06-23 22:26:57 +0000

    Committing 1 item from workspace a298258a-25c5-47b7-b809-d2c4b6246ebf

--- WH_CustomerChurn.Warehouse/gold/Tables/Dim_Churn.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [gold].[Dim_Churn] (
-
-	[Churn Key] bigint NULL, 
-	[Churn Reason] varchar(8000) NULL, 
-	[Churn Category] varchar(8000) NULL
-);
--- WH_CustomerChurn.Warehouse/gold/Tables/Dim_Contracts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [gold].[Dim_Contracts] (
-
-	[Contract Key] bigint NULL, 
-	[Contract Type] varchar(8000) NULL, 
-	[Contract Category] varchar(8000) NULL, 
-	[Payment Method] varchar(8000) NULL
-);
